22e637e964df09b188766c8240de46ff9fc797a4
Emraors <eros.martinelli@hotmail.it>
2023-12-11 16:21:59 +0100

    Refactoring

--- day03/Day03.hs
+++ day03/Day03.hs
@@ -13,23 +13,15 @@ import Test.Hspec (describe, hspec, it, shouldBe)
 
 type Coordinate = (Int, Int)
 
-{-
-data MatrixElement = MatrixElement
-  { coordinate :: Coordinate,
-    value :: Value
-  }
-  deriving (Show, Eq)
--}
-data Value = Symbol Char | Integer Int | Dot deriving (Show, Eq)
+data Value = Symbol Char | Digit Char | Dot deriving (Show, Eq)
 
 type Matrix = M.Map Coordinate Value
 
 type Dimensions = (Int, Int)
 
--- Parser for a Value
 valueParser :: Parser Value
 valueParser =
-  (Integer . read <$> many1 digit)
+  (Digit <$> digit)
     <|> (Symbol <$> satisfy (\c -> not (isDigit c) && c /= '.'))
     <|> (char '.' $> Dot)
 
@@ -37,7 +29,7 @@ testValueParser :: IO ()
 testValueParser = hspec $
   describe "valueParser" $ do
     it "parses values" $ do
-      parseOnly valueParser (pack "12") `shouldBe` Right (Integer 12)
+      parseOnly valueParser (pack "1") `shouldBe` Right (Digit '1')
       parseOnly valueParser (pack "a") `shouldBe` Right (Symbol 'a')
       parseOnly valueParser (pack ".") `shouldBe` Right Dot
 
@@ -50,8 +42,8 @@ testRowParser = hspec $
     it "parses rows" $ do
       parseOnly
         rowParser
-        (pack "12a.3%\n")
-        `shouldBe` Right [Integer 12, Symbol 'a', Dot, Integer 3, Symbol '%']
+        (pack "1a.3%")
+        `shouldBe` Right [Digit '1', Symbol 'a', Dot, Digit '3', Symbol '%']
 
 parseMatrix :: Text -> [[Value]]
 parseMatrix text = map (either error id . parseOnly rowParser) (T.lines text)
@@ -66,19 +58,20 @@ testParseMatrix =
   hspec $
     describe "parseMatrix" $
       it "parses a matrix" $
-        parseMatrix (pack "12a.3%\n2345..")
-          `shouldBe` [ [Integer 12, Symbol 'a', Dot, Integer 3, Symbol '%'],
-                       [Integer 2345, Dot, Dot]
+        parseMatrix (pack "1a.3%\n2..")
+          `shouldBe` [ [Digit '1', Symbol 'a', Dot, Digit '3', Symbol '%'],
+                       [Digit '2', Dot, Dot]
                      ]
 
--- | Returns the value at the given coordinate
-valueAt :: Matrix -> Coordinate -> Maybe Value
-valueAt m coordinate = M.lookup coordinate m
+elementAt :: Matrix -> Coordinate -> Maybe MatrixElement
+elementAt m coordinate = (,) coordinate <$> M.lookup coordinate m
 
-nearestNeighbours :: Matrix -> Coordinate -> [Value]
+type MatrixElement = (Coordinate, Value)
+
+nearestNeighbours :: Matrix -> Coordinate -> [MatrixElement]
 nearestNeighbours m (x, y) =
   catMaybes
-    [ valueAt m (x + dx, y + dy)
+    [ elementAt m (x + dx, y + dy)
       | dx <- [-1 .. 1],
         dy <- [-1 .. 1],
         not (dx == x && dy == y)
@@ -87,10 +80,10 @@ nearestNeighbours m (x, y) =
 matrix :: Matrix
 matrix =
   M.fromList
-    [ ((0, 0), Integer 1),
-      ((1, 0), Integer 2),
-      ((0, 1), Integer 3),
-      ((1, 1), Integer 4)
+    [ ((0, 0), Digit '1'),
+      ((1, 0), Digit '2'),
+      ((0, 1), Digit '3'),
+      ((1, 1), Digit '4')
     ]
 
 testNearestNeighbours :: IO ()
@@ -99,7 +92,10 @@ testNearestNeighbours =
     describe "nearestNeighbours" $
       it "returns the nearest neighbours" $
         nearestNeighbours matrix (0, 0)
-          `shouldBe` [Integer 3, Integer 2, Integer 4]
+          `shouldBe` [ ((1, 0), Digit '2'),
+                       ((0, 1), Digit '3'),
+                       ((1, 1), Digit '4')
+                     ]
 
 listToMatrix :: [[Value]] -> Matrix
 listToMatrix values =
@@ -118,7 +114,7 @@ testListToMatrix =
     describe "listToMatrix" $
       it "converts a list of lists to a matrix" $
         listToMatrix
-          [ [Integer 1, Integer 3],
-            [Integer 2, Integer 4]
+          [ [Digit '1', Digit '3'],
+            [Digit '2', Digit '4']
           ]
           `shouldBe` matrix
